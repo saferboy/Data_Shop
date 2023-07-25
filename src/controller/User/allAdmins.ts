bed44c1a01630bb6382c018306a06649596afb46
saferboy <muxtordev@gmail.com>
2023-07-25 10:16:33 +0500

    add routes /allAdmins

--- src/controller/User/allAdmins.ts
+++ src/controller/User/allAdmins.ts
@@ -1,32 +1,32 @@
-// import { Request, Response, NextFunction, query } from 'express';
-// import AuthService from '@service/user.service';
-// import { role } from '@prisma/client';
+import { Request, Response, NextFunction, query } from 'express';
+import AuthService from '@service/user.service';
+import { role } from '@prisma/client';
 
-// export default async (req: Request, res: Response, next: NextFunction) => {
-//     try {
+export default async (req: Request, res: Response, next: NextFunction) => {
+    try {
 
-//         const role: role = req.query
+        const role = req.query.role as role
 
-//         const foundAdmin = await AuthService.findAdmins(role)
+        const foundAdmin = await AuthService.findAdmins(role)
 
-//         const mapped = foundAdmin.map(admins => {
-//             return {
-//                 id: admins.id,
-//                 name: admins.name,
-//                 surname: admins.surname,
-//                 phone: admins.phone,
-//                 email: admins.email,
-//                 address: admins.address,
-//                 role: admins.role
-//             }
-//         })
+        const mapped = foundAdmin.map(admins => {
+            return {
+                id: admins.id,
+                name: admins.name,
+                surname: admins.surname,
+                phone: admins.phone,
+                email: admins.email,
+                address: admins.address,
+                role: admins.role
+            }
+        })
 
-//         return res.status(200).json({
-//             message: 'Retrive all admins',
-//             admins: mapped
-//         })
+        return res.status(200).json({
+            message: 'Retrive all admins',
+            admins: mapped
+        })
 
-//     } catch (err) {
-//         next(err);
-//     }
-// };
+    } catch (err) {
+        next(err);
+    }
+};
