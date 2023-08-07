18926dfccec39d21c72036954eb9f5487b73bb77
saferboy <muxtordev@gmail.com>
2023-08-07 21:05:48 +0500

    multer fixed

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,14 +1,20 @@
 import multer from "multer";
+import { extname } from "path";
 import { v4 as uuid } from "uuid";
 
-
+const formats = ["image/png", "image/jpg", "image/gif", "image/svg+xml"];
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, "./upload");
   },
   filename: (req, file, callback) => {
-    callback(null, uuid() + file.originalname);
+    const ext = extname(file.originalname);
+    const format = file.mimetype
+    if (!formats.includes(format)) {
+      return callback(new Error("Format error"), file.originalname)
+    }
+    callback(null, uuid() + ext)
   },
 });
 
