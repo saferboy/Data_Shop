e5a18c820fafcb487bb78c3e26f60947f995d227
saferboy <muxtordev@gmail.com>
2023-08-07 12:46:52 +0500

    update brand fixed

--- src/controller/brand/update-brand.ts
+++ src/controller/brand/update-brand.ts
@@ -28,11 +28,11 @@ export default async (req: Request, res: Response, next: NextFunction) => {
 
         const oldTitle = await BrandService.findBrandByName(title)
 
-        if (oldTitle) {
-            return res.status(200).json({
-                message: `Brand has already been created with the name: ${oldTitle.title}`
-            });
-        }
+        // if (oldTitle) {
+        //     return res.status(200).json({
+        //         message: `Brand has already been created with the name: ${oldTitle.title}`
+        //     });
+        // }
 
         const newBrand = await BrandService.updateBrandById(brandId, title, logoId)
 
--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,6 +1,8 @@
 import multer from "multer";
 import { v4 as uuid } from "uuid";
 
+
+
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, "./upload");
