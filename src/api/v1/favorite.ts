bbb33a46e26c2a58157bf128e34076e2f2b28580
saferboy <muxtordev@gmail.com>
2023-09-25 15:28:34 +0500

    all favorite routes complate

--- src/api/v1/favorite.ts
+++ src/api/v1/favorite.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import createFavorite from "@controller/favorite/create-favorite";
 import findFavorite from "@controller/favorite/find-favorite-by-id";
 import allFavorite from "@controller/favorite/all-favorite";
+import removeFavorite from "@controller/favorite/remove-favorite";
 
 import { createValidator } from "express-joi-validation"
 const validator = createValidator()
@@ -17,7 +18,7 @@ const router = Router()
     .post('/', permission(['user']), createFavorite)
     .get('/:id', permission(["user"]), validator.params(FavoriteSchema.FindFavorite), findFavorite)
     .get('/', permission(["admin", "supervisor"]), allFavorite)
-    
+    .delete('/:id', permission(["supervisor"]), removeFavorite)
 
 export default router
 
--- src/controller/favorite/remove-favorite.ts
+++ src/controller/favorite/remove-favorite.ts
@@ -1,1 +1,42 @@
 import { Request, Response, NextFunction } from 'express';
+import FavoriteService from '@service/favorite.service';
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const id = +req.params.id
+
+        const oldWishes = await FavoriteService.findFavorite(id)
+
+        if (!oldWishes?.id) {
+            return res.status(404).json({
+                message: 'Favoite product not found'
+            });
+        }
+
+        const remove = await FavoriteService.removeFavorite(id)
+
+        return res.status(200).json({
+            message: 'The product has been removed from the favorites list',
+            favorite: {
+                id: remove.id,
+                userId: remove.Product.id,
+                product: {
+                    id: remove.Product.id,
+                    title: remove.Product.title,
+                    rating: remove.Product.rating,
+                    discount: remove.Product.discount,
+                    sellPrice: remove.Product.sellPrice,
+                    image: remove.Product.file.map(item => ({
+                        id: item.id,
+                        path: item.path,
+                        filename: item.filename
+                    }))
+                }
+            }
+        });
+
+    } catch (err) {
+        next(err);
+    }
+};
